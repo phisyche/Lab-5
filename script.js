956355379743cc32c4306f04f6df327520d0ffb9
phisyche <orpheuscrypt@gmail.com>
2022-11-12 21:10:57 +0300

    Waypoints functionality

--- script.js
+++ script.js
@@ -4,5 +4,51 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/256/{z}/{x}/{y}?access_
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
-    accessToken: 'yourAccessTokenGoesHere',
+    accessToken: 'pk.eyJ1Ijoiby1ycGhldXMiLCJhIjoiY2xhZTdqcGh1MGRybDN3bzYybHZtNnB1NyJ9.MYjEhC4VLsZGi25rlSLqgQ',
+    
 }).addTo(map);
+
+var control = L.Routing.control({
+    waypoints: [
+        null
+        // L.latLng(47.246587, -122.438830),
+        // L.latLng(47.318017, -122.542970),
+        // L.latLng(47.258024, -122.444725)
+    ],
+     routeWhileDragging: true,
+     router: L.Routing.mapbox('pk.eyJ1Ijoiby1ycGhldXMiLCJhIjoiY2xhZTdqcGh1MGRybDN3bzYybHZtNnB1NyJ9.MYjEhC4VLsZGi25rlSLqgQ'),
+     units:'imperial',
+     collapsible: true,
+     geocoder: L.Control.Geocoder.photon(),
+}).addTo(map);
+
+function createButton(label, container) {
+    var btn = L.DomUtil.create('button', '', container);
+    btn.setAttribute('type', 'button');
+    btn.innerHTML = label;
+    return btn;
+}
+
+map.on('click', function(e) {
+    var container = L.DomUtil.create('div'),
+        startBtn = createButton('Start from this location', container),
+        destBtn = createButton('Go to this location', container);
+
+    L.popup()
+        .setContent(container)
+        .setLatLng(e.latlng)
+        .openOn(map); 
+    
+    L.DomEvent.on(startBtn, 'click', function() {
+        control.spliceWaypoints(0, 1, e.latlng);
+        map.closePopup();
+    });
+
+    L.DomEvent.on(destBtn, 'click', function() {
+        control.spliceWaypoints(control.getWaypoints().length - 1, 1, e.latlng);
+        control.show();
+        map.closePopup();
+    });
+ });
+
+ 
